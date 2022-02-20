76cb53330a557552d231c1d981560e446ae9f11f
Daniella Fernandes <fernandesdaniella@hotmail.com>
2022-02-20 22:01:17 +0000

    Updated product show and index actions in controller and updated show with carousel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Removing seeds..."
+Product.delete_all
+
+puts "Adding products..."
+armchair = Product.new( name: 'Armchair',
+                        category: 'Furniture',
+                        colour: 'White',
+                        material: 'Cotton')
+armchair.save!
+
+puts "Finished!"
