4155221b0a99069d48ae20ef14640fd61de21128
Daniella Fernandes <fernandesdaniella@hotmail.com>
2022-02-20 14:34:14 +0000

    Updated routes of bookings pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
-  resources :products
+  resources :products do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index]
 end
